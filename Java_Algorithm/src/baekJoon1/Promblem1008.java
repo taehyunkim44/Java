5cee239996da200a53d8e8da5bfb49474f33944c
gimtaehyeon <eiiyy12@nate.com>
2023-12-13 17:04:29 +0900

    java algorithm study

---
+++ Java_Algorithm/src/baekJoon1/Promblem1008.java
@@ -0,0 +1,16 @@
+package baekJoon1;
+
+import java.util.Scanner;
+
+public class Promblem1008 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("A 입력 : ");
+		double A = sc.nextDouble();
+		System.out.print("B 입력 : ");
+		double B = sc.nextDouble();
+		
+		System.out.println( A / B );
+	}
+}
